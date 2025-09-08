3418ca2b0d95548d2509c2d36eb87f0169dee229
Gideon-DEV-Pixel <anwanagideon18@gmail.com>
2025-09-08 14:58:20 +0100

    reimplemented regex to validate email properly

--- src/main/java/org/example/useractivitylogger/controllers/ManageStaffController.java
+++ src/main/java/org/example/useractivitylogger/controllers/ManageStaffController.java
@@ -63,7 +63,7 @@ public class ManageStaffController {
         String email = txtEmail.getText().trim();
         String password = txtPassword.getText().trim();
 
-        String EMAIL_REGEX = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$";
+        String EMAIL_REGEX = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$";
         Pattern pattern = Pattern.compile(EMAIL_REGEX);
         Matcher matcher = pattern.matcher(email);
 
